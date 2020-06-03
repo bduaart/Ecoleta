e632660ec62d5840f4c8e4489e4244b8e1165cb0
Bruno Duarte <contato.bduaart@gmail.com>
2020-06-03 13:29:43 -0300

    Added register point script

--- src/scripts/create-point.js
+++ src/scripts/create-point.js
@@ -4,7 +4,7 @@ function populateUFs() {
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
       .then(res => res.json())
       .then(states => {
-  
+
         for(state of states) {
           ufSelect.innerHTML +=`<option value="${state.id}">${state.nome}</option>`
         }
@@ -16,9 +16,8 @@ function populateUFs() {
 
 
 function getCities(event) {
-    const citiesSelect = document.querySelector("select[name=city")
+    const citySelect = document.querySelector("select[name=city")
     const stateInput = document.querySelector("input[name=state")
-    citiesSelect.innerHTML = `<option value="0">Selecione a cidade</option>`
   
     const ufValue = (event.target.value)
   
@@ -26,21 +25,69 @@ function getCities(event) {
     stateInput.value = event.target.options[indexOfSelectedState].text;
   
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+    
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
     fetch(url)
     .then(res => res.json())
     .then(cities => {
   
       for(city of cities) {
-        citiesSelect.innerHTML +=`<option value="${city.id}">${city.nome}</option>`
+        citySelect.innerHTML +=`<option value="${city.nome}">${city.nome}</option>`
       }
   
-      citiesSelect.disabled =false
+      citySelect.disabled = false
     })
   
   }
   
   
-  
   document
     .querySelector("select[name=uf")
     .addEventListener("change", getCities)
+
+
+    // Itens de coleta
+
+    const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+    for (const item of itemsToCollect){
+      item.addEventListener("click", handSelectedItem)
+    }
+
+    const collectedItems = document.querySelector("input[name=items]")
+    
+    let selectedItems = []
+
+    function handSelectedItem(event){
+        const itemLi = event.target
+      
+        itemLi.classList.toggle("selected")
+
+        const itemId = itemLi.dataset.id
+        
+        const alreadySelected = selectedItems.findIndex( item => item == itemId)
+        /*const alreadySelected = selectedItems.findIndex(item => {
+              const itemFound = item == itemId
+              return itemFound  
+        })*/
+
+        if(alreadySelected >= 0 ){
+          
+          const filteredItems = selectedItems.filter (item => item != itemId)
+          /*const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+          */ 
+
+        selectedItems = filteredItems
+        }
+      else {
+        selectedItems.push(itemId)
+
+      }
+
+      collectedItems.value = selectedItems
+    }
+
