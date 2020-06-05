4b6b85d3dbce33fc160dc60cf46214320fe5aee8
Bruno Duarte <contato.bduaart@gmail.com>
2020-06-05 08:58:54 -0300

    Created a server.js

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const server = express()
+
+server.use(express.static("public"))
+
+const nunjucks = require("nunjucks")
+
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+server.get("/", (req, res) => { 
+    res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => { 
+    res.render("create-point.html")
+})
+
+server.get("/search-results", (req, res) => {
+    res.render("search-results.html")
+})
+
+server.listen(3000)
